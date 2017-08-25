91082c51953465180b2c1d63a210326e1aabd4a0
AlexandrShestak <shestakalexandr@mail.ru>
2017-08-25 22:28:43 +0300

    Refactored

--- src/main/java/com/shestakam/philosophers/BoundedSemaphore.java
+++ src/main/java/com/shestakam/philosophers/BoundedSemaphore.java
@@ -1,21 +1,26 @@
 package com.shestakam.philosophers;
 
 public class BoundedSemaphore {
-  private int signals = 0;
-  private int bound   = 0;
+  private int signals;
+  private int bound;
 
   public BoundedSemaphore(int upperBound){
     this.bound = upperBound;
+    this.signals = 0;
   }
 
   public synchronized void take() throws InterruptedException{
-    while(this.signals == bound) wait();
+    while(this.signals == bound) {
+      wait();
+    }
     this.signals++;
     this.notify();
   }
 
   public synchronized void release() throws InterruptedException{
-    while(this.signals == 0) wait();
+    while(this.signals == 0) {
+      wait();
+    }
     this.signals--;
     this.notify();
   }
